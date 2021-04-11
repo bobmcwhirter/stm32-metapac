494e9bb3eff87853a3f4634b75460c380564507b
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2021-04-12 00:41:52 +0200

    Fix some DMA names

--- docs/stm32_metapac/dma_v2/vals/sidebar-items.js
+++ docs/stm32_metapac/dma_v2/vals/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Circ",""],["Ct",""],["Dbm",""],["Dir",""],["Dmdis",""],["Fs",""],["Fth",""],["Mburst",""],["Minc",""],["Msize",""],["Pfctrl",""],["Pincos",""],["Pl",""]]});
+initSidebarItems({"struct":[["Burst",""],["Circ",""],["Ct",""],["Dbm",""],["Dir",""],["Dmdis",""],["Fs",""],["Fth",""],["Inc",""],["Pfctrl",""],["Pincos",""],["Pl",""],["Size",""]]});
