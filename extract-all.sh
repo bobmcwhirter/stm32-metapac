b52e28d91ef178a57ed1384dc4ee87e58c4e1bc0
Bob McWhirter <bmcwhirt@redhat.com>
2021-04-19 16:09:00 -0400

    Remove the .yaml if it didn't generate.

--- extract-all.sh
+++ extract-all.sh
@@ -12,6 +12,7 @@ for f in `ls svd`; do
     RUST_LOG=info ../svd2rust/target/release/svd4rust extract-peripheral --svd svd/stm32$f.svd --transform transform.yaml --peripheral $peri > regs/$peri/$f.yaml 2> regs/$peri/$f.yaml.out
     if [ $? -ne 0 ]; then 
         mv regs/$peri/$f.yaml.out regs/$peri/$f.err
+        rm regs/$peri/$f.yaml
         echo FAIL
     else
         rm regs/$peri/$f.yaml.out
